49a854eaadca8957eea55cf6e2384640b60efc0b
luca lombardo <lucalombardo03@gmail.com>
2020-02-11 15:56:52 +0100

    Add project files.

---
+++ Esercizio Strutture Dati 3/Dati3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Esercizio_Strutture_Dati_3
+{
+    class Dati3
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserisci il nome del file sorgente: ");
+            string name = $"{ Console.ReadLine() }";
+            Console.Write("Inserisci il nome del nuovo file: ");
+            string d = $"{ Console.ReadLine() }";
+            if (File.Exists(name))
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(name))
+                    {
+                        using (StreamWriter sw = new StreamWriter(d))
+                        {
+                            string line;
+                            while ((line = sr.ReadLine()) != null)
+                            {
+                                sw.WriteLine(line);
+                            }
+                        }
+                    }
+                }
+                catch { }
+            }
+            else
+            {
+                Console.WriteLine("Non c'è il file sorgente");
+                Console.ReadLine();
+            }
+        }
+    }
+}
